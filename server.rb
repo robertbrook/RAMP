4272574c853d52c6254c0e0a17801469a62a8b22
lizconlan <Liz@liz-conlans-macbook.local>
2010-05-06 18:13:49 +0100

    refactoring

--- server.rb
+++ server.rb
@@ -12,29 +12,18 @@ MAX_NUMBER = MPS_DATA.length - 2
 
 get '/' do
   last_number = session[:num]
-  
+
   @number = params[:num]
-  unless @number
-    @number = rand(MAX_NUMBER)+1
-    while last_number == @number
-      @number = rand(MAX_NUMBER)+1
-    end
-  else
+  if @number
     @number = @number.to_i
+  else
+    @number = random_number(last_number)
   end
   
   session[:num] = @number
   
-  data_line = MPS_DATA[@number]
-  
-  unless data_line
-    raise "#{@number}"
-  end
-  
-  mp_data = FasterCSV::parse_line(data_line)
-  
-  @random_mp = MP.new(mp_data[1..2].join(" ").squeeze(" "), mp_data[3], mp_data[4], mp_data[5])
-  
+  @random_mp = setup_mp(@number)
+    
   response = @random_mp.random_photo(3)["query"]
   if response
     @results_count = response["count"].to_i
@@ -44,9 +33,7 @@ get '/' do
   
   if @results_count == 1
     @photos = [response["results"]["photo"]]
-  end
-  
-  if @results_count > 1
+  elsif @results_count > 1
     @photos = response["results"]["photo"]
   end
 
@@ -56,3 +43,19 @@ end
 get "/about" do
   haml :about
 end
+
+private
+  def random_number(last_number)
+    number = rand(MAX_NUMBER)+1
+    while last_number == number
+      number = rand(MAX_NUMBER)+1
+    end
+    number
+  end
+  
+  def setup_mp(number)
+    data_line = MPS_DATA[number]  
+    mp_data = FasterCSV::parse_line(data_line)
+    random_mp = MP.new(mp_data[1..2].join(" ").squeeze(" "), mp_data[3], mp_data[4], mp_data[5])
+    random_mp
+  end
