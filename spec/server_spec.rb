e5cc6143d636e79f95e4a83ff1e7a0d81773ada4
lizconlan <liz@work.machine>
2010-07-08 13:22:55 +0100

    mocked the call to Memcached in the spec

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -1,4 +1,5 @@
 require File.dirname(__FILE__) + '/spec_helper'
+#require 'json'
 
 describe "RAMP" do
   include Rack::Test::Methods
@@ -8,6 +9,11 @@ describe "RAMP" do
   end
 
   it "should respond to /" do
+    mock_mp = mock(MP)
+    mock_mp.stub!(:json).and_return('{"name":"Dave Smith"}')
+    mock_mp.stub!(:random_photo).and_return('{"query":{"count":"0"}}')
+    MP.should_receive(:new).and_return(mock_mp)
+    
     get '/'
     last_response.should be_ok
   end
